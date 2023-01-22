b758b94d3866efb1a2c4ec36cfe3e42f02f62e80
Abdul <abdullahmd326@gmail.com>
2023-01-22 14:22:23 -0500

    Adding UserPicture model to track a User's pictures

---
+++ backend/model/UserPicture.js
@@ -0,0 +1,20 @@
+// Adding a model to track User pictures
+const mongoose = require("mongoose");
+
+const UserPicture = new mongoose.Schema({
+    email : {
+        type: String,
+        required: true
+    },
+    search: {
+        type: String,
+        required: true
+    },
+    time : {
+        type: Date,
+        required: true,
+        default: Data.now
+    }
+});
+
+module.exports = mongoose.model("UserPicture", UserPicture);
--- backend/routes/passwordResetRoute.js
+++ backend/routes/passwordResetRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const passwordResetController = require("../controller/PasswordResetController");
 
-router.post("/", passwordResetController.PasswordResetController);
+router.post("/reset-password", passwordResetController.PasswordResetController);
 
 module.exports = passwordResetController;
