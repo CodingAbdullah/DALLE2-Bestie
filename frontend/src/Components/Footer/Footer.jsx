67481288da81d8253147f6fbca431285d9d0ef65
Abdul <abdullahmd326@gmail.com>
2023-01-23 02:28:42 -0500

    Adding Redux to footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -8,7 +8,14 @@ const Footer = () => {
     return (
         <div className="footer footer-container container">
             <footer>
-                <p className="copyright-paragraph">{ statement } <a style={{ color: 'black' }} href="https://reactjs.org/">React</a><img className="react-logo" src={require("../../assets/images/logo.svg").default} alt="logo" /></p>
+                <p className="copyright-paragraph">{ statement }
+                    <a style={{ color: 'black' }} href="https://reactjs.org/" target="_blank" rel="noreferrer">
+                        <span style={{ marginRight: '0.5rem' }}>React</span></a>
+                            and
+                    <a style={{ marginLeft: '0.5rem', color: 'black' }} href="https://redux.js.org/" target="_blank" rel="noreferrer">Redux</a>
+                    <img className="react-logo" src={require("../../assets/images/react.svg").default} alt="logo" />
+                    <img style={{ marginBottom: '0.1rem' }} className="redux-logo" src={require("../../assets/images/redux.svg").default} alt="logo" />
+                </p>
             </footer>
         </div>
     )
