674f422b61842d39245930e141e993382043942a
Abdul <abdullahmd326@gmail.com>
2023-01-28 22:32:46 -0500

    Refining search history page

--- frontend/src/Components/MyPicturesPage/MyPicturesPage.jsx
+++ frontend/src/Components/MyPicturesPage/MyPicturesPage.jsx
@@ -5,7 +5,6 @@ import { logout } from '../../redux/reducer/authReducer';
 import PictureItem from '../PictureItem/PictureItem';
 import axios from 'axios';
 
-
 const MyPicturesPage = () => {
     const userSelector = useSelector(state => state.auth.user);
 
@@ -17,7 +16,7 @@ const MyPicturesPage = () => {
     });
 
     useEffect(() => {
-        if (( userSelector || userSelector.token )  === null ) {
+        if ( userSelector === null ) {
             dispatch(logout()); // User is not allowed to search this page, clear any data
             navigate("/"); // Redirect to home page
         }
@@ -42,13 +41,8 @@ const MyPicturesPage = () => {
             });
         })
         .catch(() => {
-            dispatch(logout()); // Logout the user immediately if token is invalid or nonexistant
-            updateUserPictures((prevState) => {
-                return {
-                    ...prevState,
-                    information: null
-                }
-            });
+            dispatch(logout()); // Logout the user immediately if token is invalid or nonexistant and redirect
+            navigate("/");
         });
         }
     }, []);
@@ -61,26 +55,17 @@ const MyPicturesPage = () => {
             <div className='my-pictures-page'>
                 <h1 style={{ marginTop: '2rem' }}>Your Picture Searches</h1>
                 <div class='table-container container p-5'>
-                    <table style={{ marginTop: '1rem' }} class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">Search</th>
-                                <th scope="col">Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                userPictures.information.docs.map(pic => {
-                                    return (
-                                        <tr>
-                                            <td>{ pic.search }</td>
-                                            <td>{ pic.createdAt.split("T")[0] }</td>
-                                        </tr>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
+                    <div class="row">
+                        {
+                            userPictures.information.docs.map(pic => {
+                                return (
+                                    <div class="col-sm-12 col-md-6 col-lg-3">
+                                        <PictureItem picture={pic} />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         )
--- frontend/src/Components/PictureItem/PictureItem.jsx
+++ frontend/src/Components/PictureItem/PictureItem.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
-const PictureItem = (props) => {    
+const PictureItem = (props) => {   
+
     return (
         <div classname='picture-item'>
-            <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Search Query</h4>
-                            <p class="card-text">{props.search}</p>
-                            <h3>Date: </h3>
-                            <p class="card-text">{props.time.split("T")[0]}</p>
-                        </div>
-                    </div>
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Search Query</h4>
+                    <p class="card-text">{props.picture.search}</p>
+                    <h3>Date: </h3>
+                    <p class="card-text">{props.picture.createdAt.split("T")[0]}</p>
                 </div>
             </div>
         </div>
     )
-
 }
 
 export default PictureItem;
