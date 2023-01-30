c4cfb1365d6368362f10c0b7cd058f50fc157132
Abdul <abdullahmd326@gmail.com>
2023-01-29 23:29:46 -0500

    Footer style complete

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -12,10 +12,7 @@ const Footer = () => {
                 <p className="copyright-paragraph">{ statement }
                     <a style={styles['react-link']} href="https://reactjs.org/" target="_blank" rel="noreferrer">
                         <span style={styles['span-react']}>React</span></a>
-                            and
-                    <a style={styles['redux-link']} href="https://redux.js.org/" target="_blank" rel="noreferrer">Redux</a>
                     <img className="react-logo" style={styles['react-logo']} src={require("../../assets/images/react.svg").default} alt="logo" />
-                    <img style={styles['redux-logo']} className="redux-logo" src={require("../../assets/images/redux.svg").default} alt="logo" />
                 </p>
             </footer>
         </div>
