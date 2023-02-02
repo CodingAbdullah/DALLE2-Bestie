65e70e745c6eeb01a93ef55cdaca2a20ea998976
Abdul <abdullahmd326@gmail.com>
2023-02-02 18:25:53 -0500

    Updating ResetController to Update User, instead of Delete

--- backend/controller/PasswordResetController.js
+++ backend/controller/PasswordResetController.js
@@ -18,7 +18,7 @@ exports.PasswordResetController = (req, res) => {
                 });
             }
             else {
-                const userInfo = docs[0]; // Get the first and only user belonging to user and update password, storing the new salted/hashed version
+                // User found, reset password, update User and store the new hashed password
                 bcryptjs.genSalt(10, (err, salt) => {
                     if (err){
                         res.status(400).json({
@@ -32,40 +32,31 @@ exports.PasswordResetController = (req, res) => {
                                     message: "Cannot update password, hashing issue"
                                 });
                             }
-                            else {
-                                let newUser = new User({ email: userInfo.email, firstName: userInfo.firstName, numberOfPictures: userInfo.numberOfPictures, lastName: userInfo.lastName, password: hash });
-                                newUser.save()
-                                .then(() => {                                    
-                                    // Once verified, delete token from database and pass control to the succeeding middleware
-                                    EmailToken.deleteOne({ email })
+                            else {                                   
+                                // Once verified, delete token from database and pass control to the succeeding middleware
+                                EmailToken.deleteOne({ email })
+                                .then(() => {
+                                    User.updateOne({ email : { $set : { password : hash } }})
                                     .then(() => {
-                                        User.deleteOne({ email , password : { $ne : hash }})
-                                        .then(() => {
-                                            res.status(200).json({
-                                                message: 'Password succesfully updated and stored to db',
-                                                verified: true,
-                                                updated: true
-                                            });
-                                        })
-                                        .catch(() => {
-                                            res.status(200).json({
-                                                message: "Password successfully updated, old password not deleted from db",
-                                                verified: true,
-                                                updated: false
-                                            })
-                                        })
+                                        res.status(200).json({
+                                            message: 'Password succesfully updated and stored to db',
+                                            verified: true,
+                                            updated: true
+                                        });
                                     })
                                     .catch(() => {
                                         res.status(200).json({
-                                            message: 'Password succesfully updated, stored to db, but old password and token not removed',
-                                            verified: false,
+                                            message: "Password successfully updated, old password not deleted from db",
+                                            verified: true,
                                             updated: false
-                                        });
-                                    });
+                                        })
+                                    })
                                 })
-                                .catch(err => {
-                                    res.status(400).json({
-                                        message: "Unable to save new password to db: " + err
+                                .catch(() => {
+                                    res.status(200).json({
+                                        message: 'Password succesfully updated, stored to db, but old password and token not removed',
+                                        verified: false,
+                                        updated: false
                                     });
                                 });
                             }
