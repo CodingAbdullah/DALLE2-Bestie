725a3bb245a9aeb72dd13e5d576a8ed7ed170b41
Abdul <abdullahmd326@gmail.com>
2023-02-05 22:01:34 -0500

    Refactoring Picture Item

--- LOCAL_DEV_ENV/frontend/src/Components/PictureItem/PictureItem.jsx
+++ LOCAL_DEV_ENV/frontend/src/Components/PictureItem/PictureItem.jsx
@@ -7,8 +7,9 @@ const PictureItem = (props) => {
             <div className="card">
                 <img className="card-img-top" alt="no-text" src={`${props.picture.url}`} />
                 <div className="card-body">
-                    <h3 className="card-title">{ props.picture.search.charAt(0).toUpperCase() + props.picture.search.substring(1).toLowerCase() }</h3>
+                    <h3 className="card-title">{ props.picture.search }</h3>
                     <p style={ styles.date } className="card-text"><b>Date</b>: { props.picture.createdAt.split("T")[0] }</p>
+                    <p className="card-text"><b>Requested Size</b>: { props.picture.size.substring(0, 1).toUpperCase() + props.picture.size.substring(1).toLowerCase() }</p>
                     <p className="card-text"><b>Time</b>: { props.picture.createdAt.split("T")[1].split("Z")[0].split(".")[0] + " - GMT" }</p>
                     <button class="btn btn-danger" onClick={ props.deletePicture } value={ props.picture.url }>Delete</button>
                 </div>
