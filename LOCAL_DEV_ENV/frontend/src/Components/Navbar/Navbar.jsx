2ec548571d2f9cbee7dd151ad6f3aecc20794d8f
Abdul <abdullahmd326@gmail.com>
2023-02-05 20:05:45 -0500

    Upgrading packages, adding Bootstrap JS CDN, fixed Navbar

--- LOCAL_DEV_ENV/frontend/src/Components/Navbar/Navbar.jsx
+++ LOCAL_DEV_ENV/frontend/src/Components/Navbar/Navbar.jsx
@@ -43,15 +43,15 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid">
                 <a style={ styles['AI-Art-link'] } className="navbar-brand" href="/">DALL·E 2's Bestie</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="bg-warning navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item">
+                        <li className="nav-item dropdown">
                             <a style={ styles['link-enabled'] } className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item dropdown">
                             <a style={ styles['link-enabled'] } className="nav-link" href="/about">About/OpenAI</a>
                         </li>
                         { conditionalRenderingOnUserStateSelector }
