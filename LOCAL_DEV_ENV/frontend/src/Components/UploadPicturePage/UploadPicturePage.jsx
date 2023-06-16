c98b7500180199da383cad9ed78aff1effa6e5f1
Abdul <abdullahmd326@gmail.com>
2023-06-15 23:45:29 -0400

    Adding Upload pictures feature

---
+++ LOCAL_DEV_ENV/frontend/src/Components/UploadPicturePage/UploadPicturePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UploadPicturePage = () => {
+
+    // Code will go here...
+    return (
+        <div className="upload-picture-page">
+
+        </div>
+    )
+}
+
+
+export default UploadPicturePage;
